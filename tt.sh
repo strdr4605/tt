e1d67d96b0f927b220b2d4f342b3885dfeefee14
Dragoș Străinu <str.dr4605@gmail.com>
2021-03-20 17:59:29 +0200

    chore: print hour diff between to dates

--- tt.sh
+++ tt.sh
@@ -4,6 +4,38 @@
 #   Execute this file with a "sh" interpreter, using the "env" program search path to find it.
 # Now you know!
 
-function tt {
-  echo "Hi"
+function tt() {
+  # :- means that if TT_LOG doesn't exit, it will assign $HOME/.tt_log (~/.tt_log)
+  TT_LOG="${TT_LOG:-./.tt_log}"
+  TT_SESSION="${TT_SESSION:-./.tt_session}"
+
+  echo "Hi $TT_LOG"
+
+  if [ -f "$TT_SESSION" ]; then
+    echo "$TT_SESSION exists."
+  else
+    touch $TT_SESSION
+  fi
+
+  local timestamp=$(date +%s)
+
+  echo "session=${timestamp}" >$TT_SESSION
+
+  sleep 2
+
+  echo "$(date)" >>$TT_SESSION
+  echo "$(date -r $timestamp)" >>$TT_SESSION
+
+  # Do we have an activity active for this session?
+  local match
+  match=$(grep 'session=' "$TT_SESSION" | sed -E "s/.*session=([0-9]+).*/\\1/")
+
+  local new_timestamp=$(date +%s)
+  local sec_diff=$(($new_timestamp - $match))
+
+  local sec_in_hour=3600
+
+  local diff=$(bc <<< "scale=2; $sec_diff / $sec_in_hour")
+
+  echo "$diff"
 }
