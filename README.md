6d5203be005c61165ad6a7fd643ec935fcfc2ed5
Dragoș Străinu <str.dr4605@gmail.com>
2021-03-20 16:14:22 +0200

    feat: add tt.sh

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # tt
 
 Command line time tracker shell script
+
+## Develoment
+
+1. Change `tt.sh`
+2. `source ./tt.sh`
+3. `tt`
---
+++ tt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# First 2 chars in the file are called Shebang https://en.wikipedia.org/wiki/Shebang_%28Unix%29
+# In this case:
+#   Execute this file with a "sh" interpreter, using the "env" program search path to find it.
+# Now you know!
+
+function tt {
+  echo "Hi"
+}
