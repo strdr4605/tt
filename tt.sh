5a3995512797cd26d255b7fe0d135debc270bb5e
Dragoș Străinu <str.dr4605@gmail.com>
2021-03-21 14:19:09 +0200

    style: remove blank lines

--- tt.sh
+++ tt.sh
@@ -8,16 +8,12 @@ tt() {
   # :- means that if TT_LOGS doesn't exit, it will assign $HOME/.tt_log (~/.tt_log)
   TT_LOGS="${TT_LOGS:-$HOME/.tt_logs}"
   TT_SESSION="${TT_SESSION:-$HOME/.tt_session}"
-
   if ! [ -f "$TT_SESSION" ]; then
     touch "$TT_SESSION"
   fi
   if ! [ -f "$TT_LOGS" ]; then
     echo "UTC,activity name,time spent" >"$TT_LOGS"
   fi
-
-  echo ""
-
   if [ $# -eq 0 ]; then
     # No parameters = show help
     _options -h
@@ -27,25 +23,24 @@ tt() {
 
   # Internal functions
   _options() {
+    echo ""
     case "$1" in
-
     --activity-name)
       # Shows the current activity name if there is one for this session
       activity_name=$(grep 'activity_name=' "$TT_SESSION" | sed -E "s/.*activity_name=(.+)$.*/\\1/")
       if [ -z "${activity_name}" ]; then
         echo "No activity started"
       else
-        echo "$activity_name"
+        echo "Activity '$activity_name'"
       fi
       ;;
-
     -h | --help)
       # Help
       echo "tt - time tracker"
-      echo " "
+      echo ""
       echo "Tracks activity time with a simple start/stop syntax. Logs to CSV."
       echo "Allows one activity active at a time, per session."
-      echo " "
+      echo ""
       echo "usage: tt                                       # show this help"
       echo "usage: tt (--help or -h)                        # show this help"
       echo "usage: tt (--start or -s) [activity name]       # start a new activity"
@@ -56,29 +51,23 @@ tt() {
       echo "usage: tt --activity-name                       # show activity for current session"
       echo "usage: tt (--logs or -l)                        # show logs of previous activities"
       ;;
-
     -p | --pause)
       _pause
       ;;
-
     -d | --done | -f | --finish)
       _finish
       ;;
-
     -a | --abort)
       echo "Abort activity"
       echo "" >"$TT_SESSION"
       ;;
-
     -l | --logs)
       cat "$TT_LOGS"
       ;;
-
     --clear-logs)
       echo "Logs cleared"
       echo "UTC,activity name,time spent" >"$TT_LOGS"
       ;;
-
     -s | --start)
       _start "$2"
       ;;
@@ -88,13 +77,11 @@ tt() {
       echo ""
       _options -h
       ;;
-
     esac
   }
 
   _start() {
     start_timestamp=$(date +%s)
-
     # No activity name passed
     if [ -z "$1" ]; then
       activity_name=$(grep 'activity_name=' "$TT_SESSION" | sed -E "s/.*activity_name=(.+)$.*/\\1/")
@@ -103,7 +90,6 @@ tt() {
         return
       else
         echo "Restarting '$activity_name'"
-
         elapsed_sec=$(grep 'elapsed_sec=' "$TT_SESSION" | sed -E "s/.*elapsed_sec=([0-9]+).*/\\1/")
         echo "start_time=${start_timestamp}" >"$TT_SESSION"
         echo "elapsed_sec=${elapsed_sec}" >>"$TT_SESSION"
@@ -111,15 +97,12 @@ tt() {
       fi
       return
     fi
-
     # finish old activity if exists
     old_activity_name=$(grep 'activity_name=' "$TT_SESSION" | sed -E "s/.*activity_name=(.+)$.*/\\1/")
     if [ -n "$old_activity_name" ]; then
       _finish
     fi
-
     echo "Starting '$1'"
-
     echo "start_time=${start_timestamp}" >"$TT_SESSION"
     echo "elapsed_sec=0" >>"$TT_SESSION"
     echo "activity_name=$1" >>"$TT_SESSION"
@@ -130,25 +113,19 @@ tt() {
     start_time=$(grep 'start_time=' "$TT_SESSION" | sed -E "s/.*start_time=([0-9]+).*/\\1/")
     elapsed_sec=$(grep 'elapsed_sec=' "$TT_SESSION" | sed -E "s/.*elapsed_sec=([0-9]+).*/\\1/")
     activity_name=$(grep 'activity_name=' "$TT_SESSION" | sed -E "s/.*activity_name=(.+)$.*/\\1/")
-
     if [ -z "$start_time" ]; then
       echo "No activity started"
       return
     fi
-
     if [[ "$start_time" == "0" ]]; then
       echo "Activity '$activity_name' is already paused"
       return
     fi
-
     pause_timestamp=$(date +%s)
-
     sec_diff=$((pause_timestamp - start_time + elapsed_sec))
-
     echo "start_time=0" >"$TT_SESSION"
     echo "elapsed_sec=${sec_diff}" >>"$TT_SESSION"
     echo "activity_name=${activity_name}" >>"$TT_SESSION"
-
     echo "Activity '$activity_name' paused"
   }
 
@@ -157,22 +134,18 @@ tt() {
     start_time=$(grep 'start_time=' "$TT_SESSION" | sed -E "s/.*start_time=([0-9]+).*/\\1/")
     elapsed_sec=$(grep 'elapsed_sec=' "$TT_SESSION" | sed -E "s/.*elapsed_sec=([0-9]+).*/\\1/")
     activity_name=$(grep 'activity_name=' "$TT_SESSION" | sed -E "s/.*activity_name=(.+)$.*/\\1/")
-
     if [ -z "$activity_name" ]; then
       echo "No activity started"
       return
     fi
-
     # Activity was paused
     if [[ "$start_time" == "0" ]]; then
       _save "$activity_name" "$elapsed_sec"
       echo "" >"$TT_SESSION"
       return
     fi
-
     finish_timestamp=$(date +%s)
     sec_diff=$((finish_timestamp - start_time + elapsed_sec))
-
     _save "$activity_name" $sec_diff
     echo "" >"$TT_SESSION"
   }
@@ -182,9 +155,7 @@ tt() {
     sec_diff=$2
     hours=$((sec_diff / 3600))
     mins=$(((sec_diff - (hours * 3600)) / 60))
-
     utc_date=$(date -u)
-
     echo "$utc_date | $activity_name | ${hours}h ${mins}m"
     log="$utc_date,$activity_name,${hours}h ${mins}m"
     echo "$log" >>"$TT_LOGS"
